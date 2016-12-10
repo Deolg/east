3810cae8513eace45ff0cb2a7934f8684f4edce7
Vitalii Tereshchenko <obroshnij@gmail.com>
2016-12-10 17:54:16 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,6 @@ set :linked_dirs, %w{log tmp/pids public/uploads public/assets public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
+set :rails_env, 'production'
 # Default value for keep_releases is 5
 # set :keep_releases, 3
