76084e6f6c40398b57940897557519c7e373512b
Vitalii Tereshchenko <obroshnij@gmail.com>
2017-03-14 12:53:48 +0200

    feature/ added locale switch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-
   before_action :set_locale
-   
+
   def set_locale
     I18n.locale = params[:locale] || :ru
   end
