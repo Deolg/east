f88dd780a936891f1e81eb69742989db66f2c7e7
Unknown <sterx334@gmail.com>
2018-02-20 09:28:21 +0200

    layout "Out Service" translation of validation error Google CAPTCHA upload image in Admin panel validate img makeup photo

--- app/admin/contact.rb
+++ app/admin/contact.rb
@@ -3,7 +3,46 @@ ActiveAdmin.register Contact do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-  permit_params :phone, :skype, :viber, :mail
+  permit_params :phone, :skype, :viber, :mail, :description ,:image
+
+
+  index do
+    column :image do |obj|
+      (obj.image.present?) ? image_tag(obj.image.url(),:style=> 'width: 80px; height:80px;') : content_tag(:span, "image not uploaded")
+    end
+    column :phone
+    column :skype
+    column :viber
+    column :mail
+    column :description
+    actions
+  end
+
+  form do |f|
+    f.inputs "Contact" do
+      f.input :phone
+      f.input :skype
+      f.input :viber
+      f.input :mail
+      f.input :description
+      f.input :image , as: :file
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :phone
+      row :skype
+      row :viber
+      row :mail
+      row :description
+      row :image do |obj|
+        (obj.image.present?) ? image_tag(obj.image.url(),:style=> 'width: 300px;') : content_tag(:span, "image not uploaded")
+      end
+    end
+  end
+
 # or
 #
 # permit_params do
